3410b72f78e7bc92c474f3e58eecdea878861b1d
Jon Green <jgreen@systemsinmotion.com>
2014-10-10 22:55:25 -0400

    typo

--- src/EarthSim/Demo.java
+++ src/EarthSim/Demo.java
@@ -23,7 +23,7 @@ public class Demo {
 	static final boolean ASYNC_SIMULATION = true;
 	static final boolean ASYNC_PRESENTATION = true;
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		GUIApp presentation = new GUIApp();
 		
